d31787706ec497bb39ad3d469e891f6e69d6c1a4
Felix Tinajero <felix.tinajero@imssbienestar.gob.mx>
2026-03-11 11:22:13 -0600

    axios

--- resources/js/components/axios.js
+++ resources/js/components/axios.js
@@ -1,21 +1,19 @@
+// axios.js
 import axios from 'axios';
 
 const url = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: '/pac-check-engine/public', // <- relativo
   timeout: 300000,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
     'Accept': 'application/json',
-    // ❌ NO fijes Content-Type global
   },
   withCredentials: true,
 });
 
 url.interceptors.request.use(config => {
   const token = document.querySelector('meta[name="csrf-token"]');
-  if (token?.content) {
-    config.headers['X-CSRF-TOKEN'] = token.content;
-  }
+  if (token?.content) config.headers['X-CSRF-TOKEN'] = token.content;
   return config;
 });
 
