bc14d97507a27ec95bd94080716fadac488af68a
Felix <felix.tinajero@imssbienestar.gob.mx>
2026-07-14 12:28:57 -0600

    1

--- resources/views/layouts/app-menu.blade.php
+++ resources/views/layouts/app-menu.blade.php
@@ -10,13 +10,13 @@
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
 
-            <x-button-nav-menu active="pac" route="pac" icon="ni ni-tv-2" title="PAC" />
-            <x-button-nav-menu active="instancia" route="instancia" icon="ni ni-building" title="Instancias" />
-            <x-button-nav-menu active="tematica" route="tematica" icon="ni ni-tag" title="Temáticas" />
-            <x-button-nav-menu active="action" route="action" icon="ni ni-bullet-list-67" title="Acciones" />
+            <x-button.button-nav-menu active="pac" route="pac" icon="ni ni-tv-2" title="PAC" />
+            <x-button.button-nav-menu active="instancia" route="instancia" icon="ni ni-building" title="Instancias" />
+            <x-button.button-nav-menu active="tematica" route="tematica" icon="ni ni-tag" title="Temáticas" />
+            <x-button.button-nav-menu active="action" route="action" icon="ni ni-bullet-list-67" title="Acciones" />
 
             @if(auth()->check() && auth()->user()?->role === 'admin_oc')
-                <x-button-nav-menu active="usuarios" route="usuarios" icon="ni ni-single-02" title="Usuarios" />
+                <x-button.button-nav-menu active="usuarios" route="usuarios" icon="ni ni-single-02" title="Usuarios" />
             @endif
 
         </ul>
